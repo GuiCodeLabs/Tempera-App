63f4b56aa9a440cfaebf74991f548fe3c9023132
Pedro Henriquem <pedroxboxmx12@gmail.comm>
2026-03-26 01:56:59 -0300

    :ok_hand: style: Adição de logos e a static

--- tempera/views.py
+++ tempera/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'Tempera/home.html')
+    return render(request, 'tempera/home.html')
 
 # Create your views here.
