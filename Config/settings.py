2684e0f0d15f7bbeb4570a02cdb5192913b7087f
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-27 11:15:40 -0300

    :sparkles: style: adição do html e css do app usuarios

--- Config/settings.py
+++ Config/settings.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- usuarios/models.py
+++ usuarios/models.py
@@ -2,12 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Usuario(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
+
     foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+
     data_nascimento = models.DateField(blank=True, null=True)
     telefone = models.CharField(max_length=20, blank=True, null=True)
 
-    pass
+    def __str__(self):
+        return self.username
+
+
 
 # Create your models here.
