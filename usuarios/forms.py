fccfa0c0db3c4ba18794db6042b6927f3e635331
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-27 15:18:31 -0300

    :bug: fix: correção do bug da tela de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
 class UsuarioCadastroForm(UserCreationForm):
+    
     email = forms.EmailField(required=True)
     data_nascimento = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
 
